dfb66328d3abfcf30063148684bc40dc1f0744d0
FEPSFY6954010 <jb03293@surrey.ac.uk>
2025-10-11 22:52:04 +0100

    urn clear fix n

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ var urn = document.getElementById("urn");
 
 // Text change function
 function urnClear() {
-    urn.innerHTML = "Hi";
+    urn.innerHTML = "";
 }
 
 button1.addEventListener("click", urnClear);
