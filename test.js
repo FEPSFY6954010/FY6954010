6f254979e1351d5aa31212662b0d7e926e4b7771
FEPSFY6954010 <jb03293@surrey.ac.uk>
2025-10-11 20:44:03 +0100

    JS button test

---
+++ test.js
@@ -0,0 +1,18 @@
+// Making Identifying button from HTML code
+
+var button = document.getElementById("colour");
+
+// Making a colour change function
+
+function changeColour() {
+
+    // Random colour generation
+    var randomColour = "#" + Math.floor(Math.random()*16777215).toString(16);
+    
+    // Button colour changed
+    button.style.backgroundColor = randomColour;
+}
+
+// Adding event listener
+
+button.addEventListener("click", changeColour);
